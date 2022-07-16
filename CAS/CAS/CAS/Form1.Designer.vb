2197096d75cc202df5648f5e01b93d32ae550029
Andrew De Guzman <drewndrei@gmail.com>
2022-07-16 18:35:11 +0800

    Add login form and db verification

--- CAS/CAS/CAS/Form1.Designer.vb
+++ CAS/CAS/CAS/Form1.Designer.vb
@@ -22,10 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.cbUser = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(194, 137)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(150, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(194, 187)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(150, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'cbUser
+        '
+        Me.cbUser.FormattingEnabled = True
+        Me.cbUser.Items.AddRange(New Object() {"Student", "Admin", "Store"})
+        Me.cbUser.Location = New System.Drawing.Point(194, 71)
+        Me.cbUser.Name = "cbUser"
+        Me.cbUser.Size = New System.Drawing.Size(150, 21)
+        Me.cbUser.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(222, 272)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkVisited = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(229, 229)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(59, 13)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(568, 450)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbUser)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents cbUser As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
--- CAS/CAS/CAS/Form1.vb
+++ CAS/CAS/CAS/Form1.vb
@@ -1,3 +1,88 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    'Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Documents\CASdb.accdb")
+    Dim con As New OleDb.OleDbConnection(My.Settings.CASdbConnectionString)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+    Private Sub txtUsername_Click(sender As Object, e As EventArgs) Handles txtUsername.Click
+        txtUsername.Text = ""
+    End Sub
+
+    Private Sub txtPassword_Click(sender As Object, e As EventArgs) Handles txtPassword.Click
+        txtPassword.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If con.State = ConnectionState.Closed Then
+            con.Open()
+        End If
+
+
+        'Checking user type
+        If cbUser.SelectedItem = "Student" Then
+            'Checking complete input
+            If txtUsername.Text = String.Empty Or txtPassword.Text = String.Empty Then
+                MsgBox("Complete necessary Fields", MsgBoxStyle.Exclamation)
+            Else
+                'Checks if the user is inside the db
+                Dim cmd As New OleDbCommand("select count(*) from USERstudent where studName=? and studPassword=?", con)
+                cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = txtUsername.Text
+                cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = txtPassword.Text
+                Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+
+                If (count > 0) Then
+                    MsgBox("Login succeed", MsgBoxStyle.Information)
+                Else
+
+                    MsgBox("Account not found check credentials", MsgBoxStyle.Exclamation)
+
+                End If
+            End If
+
+        ElseIf cbUser.SelectedItem = "Admin" Then
+            'Checking complete input
+            If txtUsername.Text = String.Empty Or txtPassword.Text = String.Empty Then
+                MsgBox("Complete necessary Fields", MsgBoxStyle.Exclamation)
+            Else
+                'Checks if the user is inside the db
+                Dim cmd As New OleDbCommand("select count(*) from USERadmin where adminUsername=? and adminPassword=?", con)
+                cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = txtUsername.Text
+                cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = txtPassword.Text
+                Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+
+                If (count > 0) Then
+                    MsgBox("Login succeed", MsgBoxStyle.Information)
+                Else
+                    MsgBox("Account not found check credentials", MsgBoxStyle.Exclamation)
+                End If
+            End If
+
+        ElseIf cbUser.SelectedItem = "Store" Then
+            'Checking complete input
+            If txtUsername.Text = String.Empty Or txtPassword.Text = String.Empty Then
+                MsgBox("Complete necessary Fields", MsgBoxStyle.Exclamation)
+            Else
+                'Checks if the user is inside the db
+                Dim cmd As New OleDbCommand("select count(*) from USERstore where storeName=? and storePassword=?", con)
+                cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = txtUsername.Text
+                cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = txtPassword.Text
+                Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+
+                If (count > 0) Then
+                    MsgBox("Login succeed", MsgBoxStyle.Information)
+                Else
+                    MsgBox("Account not found check credentials", MsgBoxStyle.Exclamation)
+                End If
+            End If
+        Else
+            MsgBox("Please Select User Type")
+        End If
+
+    End Sub
 End Class
